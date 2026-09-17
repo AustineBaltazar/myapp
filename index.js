464b7f68124b1218ef0b98f95e1edda754a146fd
AustineBaltazar <Austinebaltazar.main@gmail.com>
2026-09-17 17:18:14 +0800

    initial commit of myapp

---
+++ index.js
@@ -0,0 +1,1 @@
+ const express = require('express'); const app = express(); const PORT = process.env.PORT || 3000; app.get('/', (req, res) => { res.send('Hello from my DevOps-deployed app!'); }); app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
