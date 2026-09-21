085cd554c589957f0fed189b5981f70ebcd54959
AustineBaltazar <Austinebaltazar.main@gmail.com>
2026-09-21 15:41:24 +0800

    add automated test and wire it into CI pipeline

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
- const express = require('express'); const app = express(); const PORT = process.env.PORT || 3000; app.get('/', (req, res) => { res.send('Testing Volumes now!!'); }); app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
